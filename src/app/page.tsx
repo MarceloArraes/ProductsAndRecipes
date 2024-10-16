59acf08959bd2a3fb46ed1979f8184c22dea0a28
Marcelo <marcelo.arraes@gmail.com>
2024-10-16 14:10:39 +1030

    feat: small change on home page looks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,41 +2,38 @@ import Link from "next/link";
 import { IngredientTable } from "./_components/ingredient-table";
 import { ProductTable } from "./_components/produt-table";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-// import { api } from "~/trpc/server";
 import { getServerAuthSession } from "~/server/auth";
-// import { useRouter } from "next/router";
 
-// Replace CreatePost with CreateIngredient
+interface ButtonLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const ButtonLink = ({ href, children }: ButtonLinkProps) => (
+  <Link
+    href={href}
+    className="mt-2 max-w-xs rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+  >
+    {children}
+  </Link>
+);
+
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
-  console.log("session on HOME", session);
-  // const router = useRouter();
 
-  // Replace the post API call with an ingredient equivalent, if available
-  // const helloIngredient = await api.ingredient.hello.query({ text: "from tRPC" });
+  // Basic error handling
+  if (!session) {
+    return (
+      <div className="text-red-500">Session not found. Please log in.</div>
+    );
+  }
 
   return (
     <main className="flex flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <SpeedInsights />
-      <Link
-        href={"/ingredients"}
-        className="mt-2 max-w-xs rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-      >
-        Criar Ingredients
-      </Link>
-      <Link
-        href={"/racoes"}
-        className="mt-2 max-w-xs rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-      >
-        Criar Racoes
-      </Link>
-      <Link
-        href={"/print-pdf"}
-        className="mt-2 max-w-xs rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-      >
-        Imprimir Formulacoes
-      </Link>
+      <ButtonLink href="/ingredients">Criar Ingredients</ButtonLink>
+      <ButtonLink href="/racoes">Criar Racoes</ButtonLink>
+      <ButtonLink href="/print-pdf">Imprimir Formulacoes</ButtonLink>
       <div className="mx-5 flex flex-row flex-wrap justify-around">
         <ProductTable />
         <IngredientTable />
